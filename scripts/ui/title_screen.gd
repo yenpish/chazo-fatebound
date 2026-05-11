d4e7fa312cc5d0939676eb96fcb625de6033b9cd
YenPishz <aaronhafidz2@gmail.com>
2026-05-11 19:21:39 +0800

    Add title screen

---
+++ scripts/ui/title_screen.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var start_button: Button = $Content/StartButton
+@onready var controls_label: Label = $Content/ControlsLabel
+
+
+func _ready() -> void:
+	start_button.pressed.connect(_on_start_button_pressed)
+
+	controls_label.text = "Controls:\nWASD / Arrow Keys - Move\nSpace / Left Click - Attack\nR - Restart"
+
+
+func _on_start_button_pressed() -> void:
+	DemoState.reset_demo_state()
+	get_tree().change_scene_to_file("res://scenes/levels/mossgrave_outskirts.tscn")
